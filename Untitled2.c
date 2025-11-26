2699967f1edf226dca179e10539b32102f2cf756
Qtrung474 <biunon83@gmail.com>
2025-11-26 09:29:25 +0700

    Add files via upload

---
+++ Untitled2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int findMax(int *arr, int n) {
+    int max = *arr;
+    int i;
+    for (i = 1; i < n; i++) {
+        if (*(arr + i) > max) {
+            max = *(arr + i);
+        }
+    }
+    return max;
+}
+int main() {
+    int n;
+    int i;
+    printf("Nhap so luong phan tu n: ");
+    scanf("%d", &n);
+    int arr[n];
+    for (i = 0; i < n; i++) {
+        printf("Nhap phan tu thu %d: ", i + 1);
+        scanf("%d", (arr + i));
+    }
+    int maxVal = findMax(arr, n);
+    printf("Phan tu lon nhat la: %d\n", maxVal);
+    return 0;
+}
